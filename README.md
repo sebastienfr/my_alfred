02cc2b2362c7799a59e6de941686967220865a09
Sebastien FRIESS <sebastienfr@gmail.com>
2016-03-15 00:22:13 +0100

    feat(listen): listen to keyword

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python voice recognition with pocketsphynx
+
+## About
+[Stackoverflow] (http://stackoverflow.com/questions/34596576/how-can-i-detect-one-word-with-speech-recognition-in-python)
+[Sphinx forum] (https://sourceforge.net/p/cmusphinx/discussion/help/thread/f7552e68/)
+
+
+## Install
+pip install pocketsphinx
+brew install portaudio
+pip install pyaudio
---
+++ listen.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+
+import os
+
+import pyaudio
+from pocketsphinx.pocketsphinx import *
+
+modeldir = "/usr/local/Cellar/cmu-pocketsphinx/HEAD/share/pocketsphinx/model/"
+
+# Create a decoder with certain model
+config = Decoder.default_config()
+config.set_string('-hmm', os.path.join(modeldir, 'en-us/en-us'))
+config.set_string('-dict', os.path.join(modeldir, 'en-us/cmudict-en-us.dict'))
+#config.set_string('-keyphrase', 'forward')
+#config.set_float('-kws_threshold', 1e+20)
+
+
+p = pyaudio.PyAudio()
+#TODO
+print(p.get_default_host_api_info())
+print(p.get_default_input_device_info())
+print(p.get_device_count())
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
+stream.start_stream()
+
+# Process audio chunk by chunk. On keyword detected perform action and restart search
+decoder = Decoder(config)
+decoder.set_keyphrase("kws", "jarvis")
+decoder.set_search("kws")
+decoder.start_utt()
+while True:
+    buf = stream.read(256)
+    if buf:
+        decoder.process_raw(buf, False, False)
+    else:
+        break
+    if decoder.hyp() is not None:
+        print ([(seg.word, seg.prob, seg.start_frame, seg.end_frame) for seg in decoder.seg()])
+        print ("Detected keyword, restarting search")
+        decoder.end_utt()
+        decoder.start_utt()
+#    else:
+#        print ("Nothing happened")
